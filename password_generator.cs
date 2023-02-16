9bbcc3a95cbdd105fb6091f590807fb1a2fd9955
rxfaelr <rafa331rodri@gmail.com>
2023-02-16 16:01:27 -0300

    added smtp client for sending mails

--- password_generator.cs
+++ password_generator.cs
@@ -19,8 +19,22 @@ for (int f = 0; f < 6; f++) {
 pass = pass + chars.Substring(random.Next(0, chars.Length-1), 1);
 Console.WriteLine("A senha gerada é {0}" ,pass);
 
-//
-      }   ,
+//cria uma mensagem
+System.Net.Mail.MailMessage mailMessage = new System.Net.Mail.MailMessage();
+MailMessage mail = new MailMessage();
+
+//define os endereços
+mail.From = new MailAddress(email);
+mail.To.Add(email);
+
+//define o conteúdo
+mail.Subject = "Este é um simples ,muito simples email";
+mail.Body = "Este é o corpo do email";
+
+//envia a mensagem
+SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+smtp.Send(mail); 
+      },
     }
   }
 }
